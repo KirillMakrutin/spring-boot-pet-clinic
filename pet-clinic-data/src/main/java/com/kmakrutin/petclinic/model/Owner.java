f8579a83ef1b1e46f3db62cf13a5103a9adce2c7
Kirill Makrutin <k.makrutin@gmail.com>
2018-09-29 22:13:54 +0300

    added Visit entity

--- pet-clinic-data/src/main/java/com/kmakrutin/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/kmakrutin/petclinic/model/Owner.java
@@ -1,5 +1,17 @@
 package com.kmakrutin.petclinic.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Owner extends Person
 {
+    private Set<Pet> pets = new HashSet<>();
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
