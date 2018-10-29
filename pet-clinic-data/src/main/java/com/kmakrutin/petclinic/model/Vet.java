58cfdc73632136d24ddbb8c5554e6a0b0867d93f
kmakrutin <k.makrutin@gmail.com>
2018-10-29 16:03:09 +0300

    Closes #34 Vet entity

--- pet-clinic-data/src/main/java/com/kmakrutin/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/kmakrutin/petclinic/model/Vet.java
@@ -3,15 +3,28 @@ package com.kmakrutin.petclinic.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
 public class Vet extends Person
 {
-    private Set<Specialty> specialties = new HashSet<>();
+  @ManyToMany( fetch = FetchType.EAGER )
+  @JoinTable( name = "vet_specialty",
+      joinColumns = @JoinColumn( name = "vet_id" ),
+      inverseJoinColumns = @JoinColumn( name = "specialty_id" ) )
+  private Set<Specialty> specialties = new HashSet<>();
 
-    public Set<Specialty> getSpecialties() {
-        return specialties;
-    }
+  public Set<Specialty> getSpecialties()
+  {
+    return specialties;
+  }
 
-    public void setSpecialties(Set<Specialty> specialties) {
-        this.specialties = specialties;
-    }
+  public void setSpecialties( Set<Specialty> specialties )
+  {
+    this.specialties = specialties;
+  }
 }
