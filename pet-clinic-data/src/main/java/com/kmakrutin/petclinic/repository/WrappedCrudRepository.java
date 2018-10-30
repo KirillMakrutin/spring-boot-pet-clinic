c7667e06ab1ad7d8fc3bf6cc254d544d5a36034d
kmakrutin <k.makrutin@gmail.com>
2018-10-30 13:20:45 +0300

    wrapped repository marked as not a bean

--- pet-clinic-data/src/main/java/com/kmakrutin/petclinic/repository/WrappedCrudRepository.java
+++ pet-clinic-data/src/main/java/com/kmakrutin/petclinic/repository/WrappedCrudRepository.java
@@ -3,7 +3,9 @@ package com.kmakrutin.petclinic.repository;
 import java.util.Set;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
+@NoRepositoryBean
 public interface WrappedCrudRepository<T, ID> extends CrudRepository<T, ID>
 {
   @Override
