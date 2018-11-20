f2ad149208d6d6ecc2ce8b222e03edef655c6593
kmakrutin <k.makrutin@gmail.com>
2018-11-20 12:35:30 +0300

    Fixed owner details and added visits

--- pet-clinic-web/src/main/java/com/kmakrutin/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/kmakrutin/petclinic/bootstrap/DataLoader.java
@@ -10,10 +10,12 @@ import com.kmakrutin.petclinic.model.Owner;
 import com.kmakrutin.petclinic.model.Pet;
 import com.kmakrutin.petclinic.model.Specialty;
 import com.kmakrutin.petclinic.model.Vet;
+import com.kmakrutin.petclinic.model.Visit;
 import com.kmakrutin.petclinic.service.OwnerService;
 import com.kmakrutin.petclinic.service.PetTypeService;
 import com.kmakrutin.petclinic.service.SpecialityService;
 import com.kmakrutin.petclinic.service.VetService;
+import com.kmakrutin.petclinic.service.VisitService;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -21,13 +23,15 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialityService specialityService;
+    private final VisitService visitService;
 
     @Autowired
-    public DataLoader( OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService ) {
+    public DataLoader( OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService, VisitService visitService ) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialityService = specialityService;
+      this.visitService = visitService;
     }
 
     @Override
@@ -88,6 +92,17 @@ public class DataLoader implements CommandLineRunner {
         tomPetDogGuf.setPetType(petTypeService.findByName("Dog"));
         tomPetDogGuf.setBirthDate(LocalDate.of(2013, 9, 15));
         tomPetDogGuf.setName("Guf");
+
+      Visit gufVisit1 = new Visit();
+      gufVisit1.setDescription( "Guf first visit" );
+      gufVisit1.setDate( LocalDate.now().minusDays( 2 ) );
+
+      Visit gufVisit2 = new Visit();
+      gufVisit2.setDescription( "Guf second visit" );
+      gufVisit2.setDate( LocalDate.now() );
+
+      tomPetDogGuf.getVisits().add( gufVisit1 );
+      tomPetDogGuf.getVisits().add( gufVisit2 );
 
         Pet tomPetDogFreddy = new Pet();
         tomPetDogFreddy.setPetType(petTypeService.findByName("Dog"));
